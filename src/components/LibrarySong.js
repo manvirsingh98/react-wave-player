39d1f699bbb862692ed648fc8c074fbcfb52da77
manvirsingh98 <manvirsingh98@yahoo.com>
2021-01-20 22:13:53 -0500

    added components

---
+++ src/components/LibrarySong.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const LibrarySong = ({
+  song,
+  songs,
+  setCurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelecetHandler = async () => {
+    await setCurrentSong(song);
+
+    //active song
+    const newSong = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSong);
+
+    if (isPlaying) audioRef.current.play();
+  };
+  return (
+    <div
+      onClick={songSelecetHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
+      <img src={song.cover} alt={song.name} />
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
